48401d22307f965571b8f7d14b4fc53da93b01e8
Nirvan Yang <yangnyd@gmail.com>
2025-03-28 03:42:20 -0300

    verde

--- Semaforo.asm
+++ Semaforo.asm
@@ -7,13 +7,15 @@
 	JMP EMERGENCIA ;Jump para a rotina de interrupção externa do INT0
 	ORG 000Bh      ; Endereço da interrupção do Timer 0
 	JMP TIMERSEC   ; Jump para a rotina de interrupção do Timer 0
-
+	ORG 0013h ;Endereço da interrupção INT0, que ocorre no pino 3.2
+	JMP BOTAO
 ; -------------------------------------------
 
 ; R0 = registrador utilizado para contar quando bater um segundo
 ; R1 = Registrador para identificar qual estado está para retornar (1= Verde, 2=Amarelo, 3=Vermelho)
 ; R2 = Registrador para indicar flag estado de Emergência (0 = Sem Emergência, 1 = Com Emergência)
-; 
+; R3 = Registrador para guardar a contagem de carros passando pelo sinal verde 
+;
 
 TIMERSEC:
     	MOV TH0, #11111110b   ; Recarrega TH0 com 0x3C (parte alta de 15536)
@@ -33,13 +35,17 @@ EMERGENCIA:
 	MOV TCON, #00010001b ; Liga o Timer 0 (TR0 = 1), habilita interrupção externa por borda no INT0
 	JMP QUINZE ;Jump para começar a contagem 
 
+BOTAO:
+	INC R3
+	RETI
+
 QUINZE:
 	MOV P1, #11111001b ; Exibe "1" no display
 	MOV P0, #10010010b ; Exibe "5" no display
 	CJNE R0, #1, QUINZE ; Espera R0 atingir 20
 	MOV R0, #0         ; Zera R0
 
-CATORZE: 
+CATORZE:
 	MOV P1, #11111001b ; Exibe "1" no display
 	MOV P0, #10011001b ; Exibe "4" no display
 	CJNE R0, #1, CATORZE ; Espera R0 atingir 20
@@ -62,7 +68,7 @@ ONZE:
 	MOV P0, #11111001b ; Exibe "1" no display
 	CJNE R0, #1, ONZE   ; Espera R0 atingir 20
 	MOV R0, #0         ; Zera R0
-    
+
 DEZ:
 	MOV P1, #11111001b ; Exibe "1" no display
 	MOV P0, #11000000b ; Exibe "0" no display
@@ -74,7 +80,7 @@ NOVE:
 	MOV P0, #10010000b ; Exibe "9" no display
 	CJNE R0, #1, NOVE ; Espera R0 atingir 20
 	MOV R0, #0         ; Zera R0
-    
+
 OITO:
 	MOV P1, #11000000b ; Exibe "0" no display
 	MOV P0, #10000000b ; Exibe "8" no display
@@ -92,7 +98,7 @@ SEIS:
 	MOV P0, #10000010b ; Exibe "6" no display
 	CJNE R0, #1, SEIS ; Espera R0 atingir 20
 	MOV R0, #0         ; Zera R0
-    
+
 CINCO:
 	 MOV P1, #11000000b ; Exibe "0" no display
 	 MOV P0, #10010010b ; Exibe "5" no display
@@ -122,7 +128,7 @@ UM:
 	 MOV P0, #11111001b ; Exibe "1" no display
 	 CJNE R0, #1, UM   ; Espera R0 atingir 20
 	 MOV R0, #0         ; Zera R0
-    
+
 ZERO:
 	 MOV P1, #11000000b ; Exibe "0" no display
 	 MOV P0, #11000000b ; Exibe "0" no display
@@ -138,7 +144,7 @@ ESTADO_ANTERIOR:
 AMARELO_ANTERIOR: 
 	CJNE R1, #2, VERDE ;Caso não seja estado Amarelo, é o estado Verde 
 	JMP AMARELO ;Vai para o estado Amarelo
-	
+
 INICIO:
     MOV IE, #10000011b ; Habilita interrupções (EA) 
    			; interrupção do Timer 0 (ET0), interrupção externa (INT0)
@@ -147,12 +153,20 @@ INICIO:
     MOV TMOD, #00000001b ; Configura o Timer 0 no Modo 1 (16 bits)
     MOV TCON, #00010001b ; Liga o Timer 0 (TR0 = 1), habilita interrupção externa por borda no INT0
 
+VERDE:
+	MOV R1, #1 ; Indicar que está no estado Verde
+	MOV R3, #0 ; Inicializa o contador de carros
+	MOV P2, #11111110b ;Ligar o led verde
+	JMP SETE	;Jump para iniciar a contagem de sete segundos do Estado Verde
+	CJNE R3, #5, AMARELO ; Se o número de carros não tiver alcançado 5 continua pro amarelo
+
+	MOV R3, #0 ;Zera o contador de carros
+	JMP CINCO ; Aumenta duração do sinal verde
+	JMP AMARELO ; Vai para o estado amarelo
+
 AMARELO: 
 	MOV R1, #2 ;Indicar que está no estado Amarelo 
 	MOV P2 , #11111101b ; Ligar o led amarelo
 	JMP TRES  ;Jump para Iniciar a contagem do Estado Amarelo
-
-VERDE: 
-	JMP AMARELO ;
 
 END
